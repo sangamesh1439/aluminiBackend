b6f5ff219656c4866e702504d86370d861255d38
“sangameshrently” <sangamesh.r@rently.com>
2020-01-30 12:47:12 -0800

    add jwt token

--- app.py
+++ app.py
@@ -1,14 +1,16 @@
 from flask import Flask, jsonify
 import json
 import pymongo
+from flask_jwt import JWT, jwt_required, current_identity
+from werkzeug.security import safe_str_cmp
 
 app = Flask(__name__)
 
-client = pymongo.MongoClient("mongodb+srv://<username>:<password>@cluster0-yf2bx.mongodb.net/test?retryWrites=true&w=majority")
+# client = pymongo.MongoClient("mongodb+srv://<username>:<password>@cluster0-yf2bx.mongodb.net/test?retryWrites=true&w=majority")
 
-db  = client.testdb
-for college in db.colleges.find():
-    print(college)
+# db  = client.testdb
+# for college in db.colleges.find():
+#     print(college)
 
 tasks = [
     {
@@ -28,6 +30,43 @@ tasks = [
 @app.route('/tasks', methods=['GET'])
 def getTasks():
     return jsonify({'tasks': tasks})
+
+class User(object):
+    def __init__(self, id, username, password):
+        self.id = id
+        self.username = username
+        self.password = password
+
+    def __str__(self):
+        return "User(id='%s')" % self.id
+
+users = [
+    User(1, 'user1', 'abcxyz'),
+    User(2, 'user2', 'abcxyz'),
+]
+
+username_table = {u.username: u for u in users}
+userid_table = {u.id: u for u in users}
+
+def authenticate(username, password):
+    user = username_table.get(username, None)
+    if user and safe_str_cmp(user.password.encode('utf-8'), password.encode('utf-8')):
+        return user
+
+def identity(payload):
+    user_id = payload['identity']
+    return userid_table.get(user_id, None)
+
+app = Flask(__name__)
+app.debug = True
+app.config['SECRET_KEY'] = 'super-secret'
+
+jwt = JWT(app, authenticate, identity)
+
+@app.route('/protected')
+@jwt_required()
+def protected():
+    return '%s' % current_identity
 
 if __name__ == '__main__':
    app.run(debug = True)
